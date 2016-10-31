f90b831908dc0dbfcd361a0885095793cd087c4d
Ben Carter <becarter@calpoly.edu>
2016-10-31 04:11:04 -0700

    (BC update v1.2) - added building landing page

--- CPE436_PolyMapApp/app/src/main/java/edu/calpoly/mjew/cpe436_polymapapp/entry_page.java
+++ CPE436_PolyMapApp/app/src/main/java/edu/calpoly/mjew/cpe436_polymapapp/entry_page.java
@@ -1,8 +1,11 @@
 package edu.calpoly.mjew.cpe436_polymapapp;
 
+import android.content.Intent;
 import android.support.v4.util.Pools;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.R.layout;
@@ -12,6 +15,8 @@ public class entry_page extends AppCompatActivity {
     // TODO: update buildings.xml with info from
     //      https://afd.calpoly.edu/facilities/spacefacility/name_buildings.pdf
     //  update names to be major specific?
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +28,26 @@ public class entry_page extends AppCompatActivity {
                 R.array.buildings, layout.simple_spinner_item);
         spinAdapter.setDropDownViewResource(layout.simple_spinner_dropdown_item);
         buildingList.setAdapter(spinAdapter);
+        buildingList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+
+                if (pos != 0) {
+                    String buildingInfo = parent.getItemAtPosition(pos).toString();
+                    Intent gotoMainIntent = new Intent(getApplicationContext(), buildingLandingPage.class);
+                    gotoMainIntent.putExtra("BuildingName", buildingInfo);
+                    // TODO: change this to startActivityForResult later
+                    startActivity(gotoMainIntent);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
+
+
+
 }
