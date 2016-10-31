b77e10c5d4727f8eef3527d0ab9421d7057cf7cb
Ben Carter <becarter@calpoly.edu>
2016-10-31 03:06:25 -0700

    (BC update v1.0) - login screen added

---
+++ CPE436_PolyMapApp/app/src/main/java/edu/calpoly/mjew/cpe436_polymapapp/entry_page.java
@@ -0,0 +1,13 @@
+package edu.calpoly.mjew.cpe436_polymapapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class entry_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry_page);
+    }
+}
